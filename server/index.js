042e66e926316925dce12c84f8cc83bb9ca66525
rexki1993 <s.vanree@hotmail.com>
2026-03-03 00:10:07 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -600,3 +600,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => console.log(`Shithead server listening on 0.0.0.0:${PORT}`));
+
+
